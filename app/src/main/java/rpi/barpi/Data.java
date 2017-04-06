a2d3ab9ea404032e1b2ef6906292ef3af158981f
Roceso1337 <Roceso1337@gmail.com>
2017-04-06 12:43:59 -0400

    Support for different user ids

--- app/src/main/java/rpi/barpi/Data.java
+++ app/src/main/java/rpi/barpi/Data.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Data
 {
-    public static final String VERSION="0.01";
+    public static final String VERSION="0.02";
 
     public static boolean initialized=false;
 
