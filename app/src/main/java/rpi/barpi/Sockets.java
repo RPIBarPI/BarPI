7a65e0a01c44b640fb3ebfc8483ab315262ec4a5
Roceso1337 <Roceso1337@gmail.com>
2017-04-20 13:15:38 -0400

    Fixed server ip

--- app/src/main/java/rpi/barpi/Sockets.java
+++ app/src/main/java/rpi/barpi/Sockets.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class Sockets
 {
     private static Socket sock;
-    private static final String serverIP="67.240.42.42";
+    private static final String serverIP="seanwaclawik.com";
     private static final int mainPort=42069;
     private static DataOutputStream out;
     private static DataInputStream in;
